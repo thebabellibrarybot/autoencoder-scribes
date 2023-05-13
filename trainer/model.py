929bb6cdc8394d69c0d284737399628967ca6290
thebabellibrarybot <jack@thebabellibrary.com>
2023-05-13 11:45:41 -0400

    model adj

--- trainer/model.py
+++ trainer/model.py
@@ -1,22 +1,40 @@
 from tensorflow import keras
 
-def encoder():
-    inputs = keras.Input(shape=(28, 28, 1))
-    x = keras.layers.Conv2D(32, 3, strides=2, activation='relu', padding='same')(inputs)
-    x = keras.layers.Conv2D(64, 3, strides=2, activation='relu', padding='same')(x)
-    x = keras.layers.Conv2D(128, 3, strides=2, activation='relu')(x)
-    return keras.Model(inputs, x)
-
-def decoder():
-    inputs = keras.Input(shape=(4, 4, 128))
-    x = keras.layers.Conv2DTranspose(64, 3, strides=2, activation='relu')(inputs)
-    x = keras.layers.Conv2DTranspose(32, 3, strides=2, activation='relu', padding='same')(x)
-    x = keras.layers.Conv2DTranspose(1, 3, strides=2, activation='sigmoid', padding='same')(x)
-    return keras.Model(inputs, x)
-
-def autoencoder():
-    inputs = keras.Input(shape=(28, 28, 1))
-    x = encoder()(inputs)
-    x = decoder()(x)
-    return keras.Model(inputs, x)
+# Encoder Class
+class encoder(keras.Model):
+    def __init__(self):
+        super(encoder, self).__init__()
+        self.cnn1 = keras.layers.Conv2D(32, 3, strides=2, activation='relu', padding='same')
+        self.cnn2 = keras.layers.Conv2D(64, 3, strides=2, activation='relu', padding='same')
+        self.cnn3 = keras.layers.Conv2D(128, 3, strides=2, activation='relu')
+    
+    def call(self, x):
+        x = self.cnn1(x)
+        x = self.cnn2(x)     
+        x = self.cnn3(x)        
+        return x
+# Decoder Class
+class decoder(keras.Model):
+    def __init__(self):
+        super(decoder, self).__init__()
+        self.cnnT1 = keras.layers.Conv2DTranspose(64, 3, strides=2, activation='relu')
+        self.cnnT2 = keras.layers.Conv2DTranspose(32, 3, strides=2, activation='relu', padding='same')
+        self.cnnT3 = keras.layers.Conv2DTranspose(1, 3, strides=2, activation='relu', padding='same')
+    
+    def call(self, x):
+        x = self.cnnT1(x)
+        x = self.cnnT2(x)     
+        x = self.cnnT3(x)          
+        return x
+  #Autoencoder Class
+class autoEncoder(keras.Model):
+    def __init__(self):
+        super(autoEncoder, self).__init__()
+        self.encoder = encoder()
+        self.decoder = decoder()
+    
+    def call(self, x):
+        x = self.encoder(x)
+        x = self.decoder(x)
+        return x
 
